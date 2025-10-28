4f1e2f9a2d1fdde8304ef6f849ec15413c8f7877
unknown <ghazalshabani.gca@gmail.com>
2025-10-28 04:54:32 -0700

    Modified main.py to directly launch /docs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from routes import upload, get, search, moodboard
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -7,3 +8,7 @@ app.include_router(upload.router)
 app.include_router(get.router)
 app.include_router(search.router)
 app.include_router(moodboard.router)
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
